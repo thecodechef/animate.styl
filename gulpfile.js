43c154be0e402bb60003be56eff69ad66810a8b9
Jeremy Bolding <cyberchefjay@gmail.com>
2015-04-05 03:05:01 -0500

    Fixed Structure to Resemble Animate.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,37 @@
 'use strict';
-
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    stylus = require('gulp-stylus'),
-    prefix = require('gulp-autoprefixer'),
-    csso = require('gulp-csso'),
-    rename = require('gulp-rename'),
-    sourcemaps = require('gulp-sourcemaps'),
-    concat = require('gulp-concat'),
-    del = require('del');
-
-var config = require('./config.json'),
-    target = ['./animations/base.styl'],
-    count = 0;
-
-var animDest = './',
-    animCss = '/animate.css',
-    animStyl = '/animate.styl';
-
-gulp.task('clean',function(c,b,a){
-    del('./animate.styl',a);
-    del('./animate.css',b);
-    del('./animate.min.css',c);
-});
-
-gulp.task('build',function(){
-        var catagories = config,
-            catagory,files,file;
+var gulp = require('gulp');
+var $_ = require('gulp-load-plugins')({rename: {'gulp-autoprefixer':'prefix'}});
+var gutil = require('gulp-util');
+var fs = require('fs-extra');
+gulp.task('default', function(){
+    fs.readJson('./config.json',function(err,data) {
+        if(err) {console.warn(err);}
+        var catagories = data;
+        var catagory,file,files;
+        var target = ['./source/base.styl'],
+            count = 0;
         for (catagory in catagories) {
-            if (catagories.hasOwnProperty(catagory)) {
+            if(catagories.hasOwnProperty(catagory)) {
                 files = catagories[catagory];
-                for (file in files) {
-                    if (files.hasOwnProperty(file) && files[file]) {
-                        target.push('./animations/' + catagory + '/' + file + '.styl');
-                        count += 1;
-                    }
+                for(file in files) {
+                    if(files.hasOwnProperty(file) && files[file]) {
+                        target.push('./source/'+catagory+'/'+file+'.styl');
+                        count +=1;                    }
                 }
             }
         }
-        if (!count) {
-            gutil.log(gutil.colors.red('======================='));
-            gutil.log(gutil.colors.red('No animations activated.'));
-            gutil.log(gutil.colors.red('======================='));
-        }else {
-            gutil.log(gutil.colors.yellow('======================='));
-            gutil.log(gutil.colors.yellow(count + (count <= 1 ? ' Animation' : ' Animations') + ' Activated'));
-            gutil.log(gutil.colors.yellow('======================='));
-        }
-    gulp.src(target)
-        .pipe(concat('animate.styl'))
-        .pipe(gulp.dest(animDest));
+        target.push('./source/keyframes.styl');
+        if (!count) {gutil.log(gutil.colors.red('Error | No Animations Activated!'));}
+        if (count) {gutil.log(gutil.colors.green('Success | '+count+(count <= 1 ? ' Animation' : ' Animations')+' Activated'));}
+        gulp.src(target)
+            .pipe($_.concat('animate.styl'))
+            .pipe(gulp.dest('./'))
+            .pipe($_.stylus())
+            .pipe($_.prefix('last 1 versions','bb 10'))
+            .pipe(gulp.dest('./'))
+            .pipe($_.csso())
+            .pipe($_.rename({suffix:'.min'}))
+            .pipe(gulp.dest('./'));
+        gulp.watch('./source/**/*.styl',['default']);
+    });
 });
-
-gulp.task('cssify',function() {
-    gulp.src(animDest + animStyl)
-        .pipe(sourcemaps.init())
-        .pipe(stylus())
-        .pipe(prefix({browsers:['last 2 versions', 'bb 10'], cascade: true}))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(animDest));
-});
-
-gulp.task('minify', function() {
-    gulp.src(animDest + animCss)
-        .pipe(csso())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(animDest));
-});
-
-gulp.task('default', function(){});
