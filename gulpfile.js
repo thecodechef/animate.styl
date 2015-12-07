936fd7e08d5d1d1e3af090d4fa6643f3cca7bd67
Jeremy Bolding <cyberchefjay@gmail.com>
2015-12-07 04:53:34 -0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 'use strict';
+
 var gulp = require('gulp');
 var $_ = require('gulp-load-plugins')({rename: {'gulp-autoprefixer':'prefix'}});
+
 gulp.task('default', function(){
     gulp.src('./config.yml').pipe($_.yaml({space:4})).pipe(gulp.dest('./'));
+    
     var config = require('./config.json');
     var catagories = config,
+    
     catagory,files,file,
     target = ['./source/base.styl'],
     count = 0;
+   
     for (catagory in catagories) {
         if(catagories.hasOwnProperty(catagory)){
             files = catagories[catagory];
@@ -19,9 +24,21 @@ gulp.task('default', function(){
             }
         }
     }
+   
     target.push('./source/keyframes.styl');
-    if (!count) {console.log('Error | No Animations Activated!');}if (count) {console.log('Success | '+count+(count <= 1 ? ' Animation' : ' Animations')+' Activated');}
-    gulp.src(target).pipe($_.concat('animate.styl')).pipe(gulp.dest('./'))
-    .pipe($_.stylus()).pipe($_.prefix('last 1 version','bb 10')).pipe(gulp.dest('./'))
-    .pipe($_.csso()).pipe($_.rename({suffix: '.min'})).pipe(gulp.dest('./'));
+   
+    if (!count) {console.log('Error | No Animations Activated!');}
+    if (count) {
+        console.log('Success | '+count+(count <= 1 ? ' Animation' : ' Animations')+' Activated');
+    }
+   
+    gulp.src(target)
+      .pipe($_.concat('animate.styl'))
+      .pipe(gulp.dest('./'))
+      .pipe($_.stylus())
+      .pipe($_.prefix('last 1 version','bb 10'))
+      .pipe(gulp.dest('./'))
+      .pipe($_.csso())
+      .pipe($_.rename({suffix: '.min'}))
+      .pipe(gulp.dest('./'));
 });
